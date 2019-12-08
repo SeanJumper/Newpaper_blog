9d3916d3e844155a6e70f19024398c1703eec62f
beavish <kieran.philip.williams@gmail.com>
2019-12-08 15:30:39 +0000

    commenting on post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
-before_action :authenticate_user! ,only:[ :index,:new, :edit, :update, :destroy]
-before_action :authenticate_admin!, only:[:new, :edit, :update, :destroy]
-before_action :set_post, only: [:show, :edit, :update, :destroy]
+#before_action :authenticate_user! ,only:[ :index,:new, :edit, :update, :destroy]
+#before_action :authenticate_admin!, only:[:new, :edit, :update, :destroy]
+#before_action :set_post, only: [:show, :edit, :update, :destroy]
 def authenticate_admin!
   if user_signed_in?
   # check if current user is admin
